2446296aa5092e6ac03fefec8f36d1618f62998f
Matt A Harrison <matt.anthony.harrison@gmail.com>
2018-10-25 17:26:35 +0100

    Invokes server at fixes hostname and maildrop

--- src/Pop3_server.ml
+++ src/Pop3_server.ml
@@ -1,1 +1,11 @@
-let () = ()
+open Pop3.Server
+open Pop3.State
+open Pop3.Store
+
+module B = BackingStoreState (GmTimeBanner) (IrminStore)
+
+module S = Server (B)
+
+let () =
+  let stop, _resolver = Lwt.wait () in
+  Lwt_main.run (S.start ~hostname:"localhost" ~maildrop:"/tmp/pop3" ~stop)
